bae9f227bdf8cca74075924295c64aca20d83326
Fedor Gusev <gusevfedya@mail.ru>
2020-09-13 17:15:56 +0300

    add types

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { TypeComponent } from './type/type.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'type', component: TypeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
